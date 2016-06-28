05d5d07be2a4620c2a0cd79adef73b1a698c5aa6
Luis Manuel Ramirez Vargas <luis.manuel.ramirez.91@gmail.com>
2016-06-27 20:45:31 -0500

    Add Image to CollectionViewCell

---
+++ UICollectionView/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  UICollectionView
+//
+//  Created by Luis Manuel Ramirez Vargas on 27/06/16.
+//  Copyright © 2016 Luis Manuel Ramirez Vargas. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
