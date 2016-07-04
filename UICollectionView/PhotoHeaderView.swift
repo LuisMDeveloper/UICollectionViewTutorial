35181e3f01754f217a6fb58d80e354d256deb299
Luis Manuel Ramirez Vargas <luis.manuel.ramirez.91@gmail.com>
2016-07-03 20:31:12 -0500

    Using sections in collection view

---
+++ UICollectionView/PhotoHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoHeaderView.swift
+//  UICollectionView
+//
+//  Created by Luis Manuel Ramirez Vargas on 03/07/16.
+//  Copyright © 2016 Luis Manuel Ramirez Vargas. All rights reserved.
+//
+
+import UIKit
+
+class PhotoHeaderView: UICollectionReusableView {
+        
+}
